61580e41e77e6648d684a0e18c4db70032a0bedc
King Zhou <zhouyimingking@outlook.com>
2021-11-19 13:11:44 -0800

    Changed basic brick back to square support custom brick loading (amount, area) slightly decreases ball speed

--- Assets/_Script/GameManager.cs
+++ Assets/_Script/GameManager.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -8,13 +9,14 @@ public class GameManager : MonoBehaviour
 {
     [SerializeField] private GameObject brickPrefab;
     [SerializeField] private int autoSaveTime;
+    [SerializeField] private Transform[] spawnAnchors;
+    [SerializeField] private int amountInaRow, amountInaCol;
     
     public static GameManager Instance { get; private set; }
     public bool InLaunchPrep { get; set; }
     public Camera MainCam { get; private set; }
 
     private List<GameObject> _brickPool;
-
     private int _score;
     private Guid _gameGuid;
     private int _countdown;
@@ -64,13 +66,19 @@ public class GameManager : MonoBehaviour
     private void InstantiateBlocks()
     {
         _brickPool = new List<GameObject>();
+        var x1 = spawnAnchors[0].position.x;
+        var y1 = spawnAnchors[0].position.y;
+        var x2 = spawnAnchors[1].position.x;
+        var y2 = spawnAnchors[1].position.y;
 
+        // ReSharper disable once TooWideLocalVariableScope
         GameObject temp;
-        for (var i = -10; i <= 10; i++)
+        for (var i = 0; i < amountInaRow; i++)
         {
-            for (var j = -10; j <= 10; j++)
+            for (var j = 0; j < amountInaCol; j++)
             {
-                temp = Instantiate(brickPrefab, new Vector2((float) i / 5, (float) j / 4), Quaternion.identity);
+                var pos = new Vector2(x1 + i * (x2 - x1) / (amountInaRow - 1), y1 + j * (y2 - y1) / (amountInaCol - 1));
+                temp = Instantiate(brickPrefab, pos, Quaternion.identity);
                 _brickPool.Add(temp);
             }
         }
